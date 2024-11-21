c601b21e73e20b49ef75d868a83006a311cae286
Minh Tuyen <bachminhtuyen2003@gmail.com>
2024-11-21 11:02:07 +0700

    config(api): install & config WebAPI Package

--- E-Commerce_Coffee_And_Tea_Client/Global.asax.cs
+++ E-Commerce_Coffee_And_Tea_Client/Global.asax.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using E_Commerce_Coffee_And_Tea_Client.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
 using Unity;
@@ -13,6 +15,7 @@ namespace E_Commerce_Coffee_And_Tea_Client
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             UnityConfig.RegisterComponents();
         }
